f2247f7d5678d74e736079a9355e68cf73f1eda0
Nester Okertchiri <naokertchiri@gmail.com>
2022-12-09 16:33:22 +0000

    git init

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["*.html", "./assets/js/main.js"],
+  theme: {
+    extend: {
+      inset: {
+        100: "100%",
+      },
+
+      padding: {
+        120: "120px",
+      },
+
+      colors: {
+        primary: "#004C7F",
+        secondary: "#F8A527",
+        neutral: "#5B5B5B",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};

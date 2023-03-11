73cd27f08d52e33538ddc2225eeae2873a405fcf
Nester Okertchiri <naokertchiri@gmail.com>
2023-03-11 04:51:34 +0000

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,74 @@
-$(function () {
+(function () {
   "use strict";
 
-  //===== Prealoder
+  // ======= Sticky
+  window.onscroll = function () {
+    const ud_header = document.querySelector(".ud-header");
+    const top_banner = document.querySelector(".top-banner");
+    const sticky = ud_header.offsetTop;
+    const logo = document.querySelector(".header-logo");
 
-  $(".preloader").delay(500).fadeOut(500);
+    // if (window.pageYOffset > sticky) {
+    //   top_banner.classList.add("hidden");
+    // } else {
+    //   top_banner.classList.remove("hidden");
+    // }
 
-  //===== Sticky
+    // === logo change
+    // if (ud_header.classList.contains("sticky")) {
+    //   logo.src = "assets/images/logo/uplogo2.svg";
+    // } else {
+    //   logo.src = "assets/images/logo/uplogo2.svg";
+    // }
 
-  $(window).on("scroll", function (event) {
-    var scroll = $(window).scrollTop();
-    if (scroll < 20) {
-      $(".navbar-area").removeClass("sticky");
-      $(".navbar .navbar-brand img").attr("src", "assets/images/logo.svg");
+    // show or hide the back-top-top button
+    const backToTop = document.querySelector(".back-to-top");
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      backToTop.style.display = "flex";
     } else {
-      $(".navbar-area").addClass("sticky");
-      $(".navbar .navbar-brand img").attr("src", "assets/images/logo-2.svg");
+      backToTop.style.display = "none";
     }
-  });
+  };
 
-  //===== Section Menu Active
+  // ===== responsive navbar
+  let navbarToggler = document.querySelector("#navbarToggler");
+  const navbarCollapse = document.querySelector("#navbarCollapse");
 
-  var scrollLink = $(".page-scroll");
-  // Active link switching
-  $(window).scroll(function () {
-    var scrollbarLocation = $(this).scrollTop();
-
-    scrollLink.each(function () {
-      var sectionOffset = $(this.hash).offset().top - 73;
-
-      if (sectionOffset <= scrollbarLocation) {
-        $(this).parent().addClass("active");
-        $(this).parent().siblings().removeClass("active");
-      }
-    });
+  navbarToggler.addEventListener("click", () => {
+    navbarToggler.classList.toggle("navbarTogglerActive");
+    navbarCollapse.classList.toggle("hidden");
   });
 
   //===== close navbar-collapse when a  clicked
+  document
+    .querySelectorAll("#navbarCollapse ul li:not(.submenu-item) a")
+    .forEach((e) =>
+      e.addEventListener("click", () => {
+        navbarToggler.classList.remove("navbarTogglerActive");
+        navbarCollapse.classList.add("hidden");
+      })
+    );
 
-  $(".navbar-nav a").on("click", function () {
-    $(".navbar-collapse").removeClass("show");
+  // ===== Sub-menu
+  const submenuItems = document.querySelectorAll(".submenu-item");
+  submenuItems.forEach((el) => {
+    el.querySelector("a").addEventListener("click", () => {
+      el.querySelector(".submenu").classList.toggle("hidden");
+    });
   });
+  
+  
 
-  $(".navbar-toggler").on("click", function () {
-    $(this).toggleClass("active");
-    $(".navbar-collapse").toggleClass("show");
-  });
+  //====== header scrolling content
 
-  $(".navbar-nav a").on("click", function () {
-    $(".navbar-toggler").removeClass("active");
-  });
-
-  var title = ["- Engage", "- Accelerate", "- Scale", "- Sustain"];
+  var title = ["Engage ", "Accelerate ", "Sustain "];
   var subtitle = [
     "our agile software development team to co-create or update your digital product with the latest web, mobile and cloud technologies.",
-    "your businesses growth with our proven development servicess and products to achieve zero latency.",
-    "your company capabilities by decresing technology costs without losing on high quality.",
-    "your top business level with our software infrustructure and delivery services.",
+    "your businesses growth with our proven development and products to achieve zero latency and quality services.",
+    "your company capabilities by decresing technology costs without losing on high quality software infrustructure.",
   ];
 
   var counter = 1;
@@ -63,13 +76,13 @@ $(function () {
   var inst = setInterval(textSequence, 8000);
 
   function textSequence() {
-    $(".sequence").removeClass("lineUp");
+    document.querySelector(".sequence").classList.remove("lineUp");
 
     setTimeout(function () {
-      $(".sequence").addClass("lineUp");
+      document.querySelector(".sequence").classList.add("lineUp");
 
-      document.getElementById("title-sequence").innerHTML = title[counter];
-      document.getElementById("subtitle-sequence").innerHTML =
+      document.querySelector("#title-sequence").innerHTML = title[counter];
+      document.querySelector("#subtitle-sequence").innerHTML =
         subtitle[counter];
 
       counter++;
@@ -79,297 +92,53 @@ $(function () {
       }
     }, 1000);
   }
-  // }
 
-  //===== testimonial active
-
-  $(".testimonial-active").slick({
-    dots: true,
-    speed: 800,
-    arrows: false,
-    centerMode: true,
-    centerPadding: "0",
-    slidesToShow: 3,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 3,
-        },
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          slidesToShow: 2,
-          centerMode: false,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-      {
-        breakpoint: 576,
-        settings: {
-          slidesToShow: 1,
-        },
-      },
-    ],
+  // ===== Faq accordion
+  const faqs = document.querySelectorAll(".single-faq");
+  faqs.forEach((el) => {
+    el.querySelector(".faq-btn").addEventListener("click", () => {
+      el.querySelector(".icon").classList.toggle("-rotate-90");
+      el.querySelector(".faq-content").classList.toggle("hidden");
+    });
   });
 
-  //===== Back to top
-
-  // Show or hide the sticky footer button
-  $(window).on("scroll", function (event) {
-    if ($(this).scrollTop() > 600) {
-      $(".back-to-top").fadeIn(200);
-    } else {
-      $(".back-to-top").fadeOut(200);
-    }
-  });
-
-  //Animate the scroll to yop
-  $(".back-to-top").on("click", function (event) {
-    event.preventDefault();
-
-    $("html, body").animate(
-      {
-        scrollTop: 0,
-      },
-      1500
-    );
-  });
-
-  //=====  WOW active
-
+  // ===== wow js
   new WOW().init();
 
-  //=====  particles
+  // ====== scroll top js
+  function scrollTo(element, to = 0, duration = 500) {
+    const start = element.scrollTop;
+    const change = to - start;
+    const increment = 30;
+    let currentTime = 0;
 
-  if (document.getElementById("particles-1"))
-    particlesJS("particles-1", {
-      particles: {
-        number: {
-          value: 40,
-          density: {
-            enable: !0,
-            value_area: 4000,
-          },
-        },
-        color: {
-          value: ["#FFFFFF", "#FFFFFF", "#FFFFFF"],
-        },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#fff",
-          },
-          polygon: {
-            nb_sides: 5,
-          },
-          image: {
-            src: "img/github.svg",
-            width: 33,
-            height: 33,
-          },
-        },
-        opacity: {
-          value: 0.15,
-          random: !0,
-          anim: {
-            enable: !0,
-            speed: 0.2,
-            opacity_min: 0.15,
-            sync: !1,
-          },
-        },
-        size: {
-          value: 50,
-          random: !0,
-          anim: {
-            enable: !0,
-            speed: 2,
-            size_min: 5,
-            sync: !1,
-          },
-        },
-        line_linked: {
-          enable: !1,
-          distance: 150,
-          color: "#ffffff",
-          opacity: 0.4,
-          width: 1,
-        },
-        move: {
-          enable: !0,
-          speed: 1,
-          direction: "top",
-          random: !0,
-          straight: !1,
-          out_mode: "out",
-          bounce: !1,
-          attract: {
-            enable: !1,
-            rotateX: 600,
-            rotateY: 600,
-          },
-        },
-      },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: {
-            enable: !1,
-            mode: "bubble",
-          },
-          onclick: {
-            enable: !1,
-            mode: "repulse",
-          },
-          resize: !0,
-        },
-        modes: {
-          grab: {
-            distance: 400,
-            line_linked: {
-              opacity: 1,
-            },
-          },
-          bubble: {
-            distance: 250,
-            size: 0,
-            duration: 2,
-            opacity: 0,
-            speed: 3,
-          },
-          repulse: {
-            distance: 400,
-            duration: 0.4,
-          },
-          push: {
-            particles_nb: 4,
-          },
-          remove: {
-            particles_nb: 2,
-          },
-        },
-      },
-      retina_detect: !0,
-    });
+    const animateScroll = () => {
+      currentTime += increment;
 
-  if (document.getElementById("particles-2"))
-    particlesJS("particles-2", {
-      particles: {
-        number: {
-          value: 40,
-          density: {
-            enable: !0,
-            value_area: 4000,
-          },
-        },
-        color: {
-          value: ["#FFFFFF", "#FFFFFF", "#FFFFFF"],
-        },
-        shape: {
-          type: "circle",
-          stroke: {
-            width: 0,
-            color: "#fff",
-          },
-          polygon: {
-            nb_sides: 5,
-          },
-          image: {
-            src: "img/github.svg",
-            width: 33,
-            height: 33,
-          },
-        },
-        opacity: {
-          value: 0.15,
-          random: !0,
-          anim: {
-            enable: !0,
-            speed: 0.2,
-            opacity_min: 0.15,
-            sync: !1,
-          },
-        },
-        size: {
-          value: 50,
-          random: !0,
-          anim: {
-            enable: !0,
-            speed: 2,
-            size_min: 5,
-            sync: !1,
-          },
-        },
-        line_linked: {
-          enable: !1,
-          distance: 150,
-          color: "#ffffff",
-          opacity: 0.4,
-          width: 1,
-        },
-        move: {
-          enable: !0,
-          speed: 1,
-          direction: "top",
-          random: !0,
-          straight: !1,
-          out_mode: "out",
-          bounce: !1,
-          attract: {
-            enable: !1,
-            rotateX: 600,
-            rotateY: 600,
-          },
-        },
-      },
-      interactivity: {
-        detect_on: "canvas",
-        events: {
-          onhover: {
-            enable: !1,
-            mode: "bubble",
-          },
-          onclick: {
-            enable: !1,
-            mode: "repulse",
-          },
-          resize: !0,
-        },
-        modes: {
-          grab: {
-            distance: 400,
-            line_linked: {
-              opacity: 1,
-            },
-          },
-          bubble: {
-            distance: 250,
-            size: 0,
-            duration: 2,
-            opacity: 0,
-            speed: 3,
-          },
-          repulse: {
-            distance: 400,
-            duration: 0.4,
-          },
-          push: {
-            particles_nb: 4,
-          },
-          remove: {
-            particles_nb: 2,
-          },
-        },
-      },
-      retina_detect: !0,
-    });
-});
+      const val = Math.easeInOutQuad(currentTime, start, change, duration);
+
+      element.scrollTop = val;
+
+      if (currentTime < duration) {
+        setTimeout(animateScroll, increment);
+      }
+    };
+
+    animateScroll();
+  }
+
+  Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
+  };
+
+  document.querySelector(".back-to-top").onclick = () => {
+    scrollTo(document.documentElement);
+  };
+
+  
+})();
+
+
